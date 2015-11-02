0b8923194406ae07f7c587ee831a3e63885b2e7f
GE60 <GE60@10.0.0.6>
2015-11-01 21:31:55 -0500

    updated main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,89 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import application.PokerTableController;
+import application.RootPokerController;
 
 public class Main extends Application {
+
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
+	
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
+		
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root, 400, 400);
+
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Poker");
+
+		// Set the application icon.
+		this.primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/res/img/26.png")));
+
+		this.primaryStage.setScene(scene);
+		this.primaryStage.show();
+
+		initRootLayout();
+
+		showPokerTable();
+
+	}
+
+	public void initRootLayout() {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
+
+			// Give the controller access to the main app.
+			RootPokerController controller = loader.getController();
+			controller.setMainApp(this);
+
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void showPokerTable() {
+		try {
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/PokerTable.fxml"));
+			AnchorPane personOverview = (AnchorPane) loader.load();
+
+			// Set person overview into the center of root layout.
+			rootLayout.setCenter(personOverview);
+
+			// Give the controller access to the main app.
+			PokerTableController controller = loader.getController();
+			controller.setMainApp(this);
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		launch(args);
+		launch(args);		
 	}
+	
+	
 }
